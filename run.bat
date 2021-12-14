8d80f5e5f2adf5e4ff066d377e69825b2e8bc89b
jjbhatewara <jinesh.21810002@viit.ac.in>
2021-12-14 12:17:00 +0530

    Run bat modified

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 c:\
 @REM cd c:/venvs/amry/Scripts/Activate
-start c:/venvs/amry/Scripts/python.exe "c:/Users/DELL1/Desktop/Desktop/TY/Other/ARMY PROJECT/dev/AMRY/manage.py " runserver 0.0.0.0:8000
+start ../venvs/amry/Scripts/python.exe "manage.py " runserver 0.0.0.0:8000
 @REM start python manage.py runserver
 timeout 10
 @REM start http://127.0.0.1:8000/
